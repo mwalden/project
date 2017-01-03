30bedecd55f6ea7692c87eeb4db565be80270c2d
mwalden <tont0r@hotmail.com>
2017-01-03 12:58:19 -0500

    2nd first push

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using AC;
+
+public class ChangeScene : MonoBehaviour {
+	public Cutscene myCutScene;
+
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			myCutScene.Interact ();
+		}
+	}
+
+	void OnMouseDown(){
+		myCutScene.Interact ();
+	}
+}
---
+++ Assets/Scripts/ChangeSceneViaInteraction.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using AC;
+
+public class ChangeSceneViaInteraction : MonoBehaviour {
+	public Interaction interaction;
+	public float seconds;
+	private bool loading;
+	void Update(){
+		seconds -= Time.deltaTime;
+		if (seconds < 0 && !loading) {
+			loading = true;	
+			interaction.Interact ();
+		}
+	}
+
+}
